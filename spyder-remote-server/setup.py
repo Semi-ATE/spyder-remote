74e1925f7f155e679a11d12305e6423e474c7eb5
goanpeca <goanpeca@gmail.com>
2020-09-08 20:25:36 -0500

    Update with bump2version

--- spyder-remote-server/setup.py
+++ spyder-remote-server/setup.py
@@ -1,11 +1,34 @@
 # Copyright (c) Tom Hören.
 # Distributed under the terms of the MIT License.
+
+# Standard library imports
+import ast
+import os
+
+# Third party imports
 from setuptools import find_packages, setup
+
+# Constants
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version(module='spyder_remote_server'):
+    """Get version."""
+    with open(os.path.join(HERE, module, '__init__.py'), 'r') as f:
+        data = f.read()
+
+    lines = data.split('\n')
+    for line in lines:
+        if line.startswith('__version__'):
+            version = ast.literal_eval(line.split('=')[-1].strip())
+            break
+
+    return version
 
 
 setup(
     name="spyder-remote-server",
-    version="0.1.0",
+    version=get_version(),
     url="https://github.com/Semi-ATE/spyder-remote/tree/master/spyder-remote-server",
     description="",
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
--- spyder-remote-server/spyder_remote_server/__init__.py
+++ spyder-remote-server/spyder_remote_server/__init__.py
@@ -1,2 +1,4 @@
 # Copyright (c) Tom Hören.
 # Distributed under the terms of the MIT License.
+
+__version__ = "0.0.0"
